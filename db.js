e71332b236573541016429bdf035368ce3192ed4
Abhinav Mahalley <85456745+AbhinavMahalley@users.noreply.github.com>
2022-02-05 23:40:55 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,22 @@
+// Open a database
+// create objectstore -> created/modify only in upgradeneeded only
+// make transactions
+let db;
+let openRequest = indexedDB.open("myDataBase");
+openRequest.addEventListener("success", (e) => {
+    console.log("DB Success");
+    db = openRequest.result;
+})
+
+openRequest.addEventListener("error", (e) => {
+    console.log("DB error");
+})
+
+openRequest.addEventListener("upgradeneeded", (e) => {
+    console.log("DB upgraded and also for initial DB creation");
+    db = openRequest.result;
+
+    db.createObjectStore("video", { keyPath : "id" });
+    db.createObjectStore("image", { keyPath : "id" });
+
+})
